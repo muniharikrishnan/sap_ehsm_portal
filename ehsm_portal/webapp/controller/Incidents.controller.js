05ec21ecc0102f7f75a11ac8be08d0846b8c4142
Muni Hari Krishnan M <amruthavallia7@gmail.com>
2025-08-24 14:15:22 +0530

    updated all the css including risk and incident

--- ehsm_portal/webapp/controller/Incidents.controller.js
+++ ehsm_portal/webapp/controller/Incidents.controller.js
@@ -1,13 +1,44 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("ehsmportal.controller.Incidents", {
-		onInit: function () {},
+		onInit: function () {
+			// Store reference to the table for filtering
+			this._oTable = this.byId("tblIncidents");
+			this._oSearchField = this.byId("incidentSearchField");
+		},
 
 		onBack: function () {
 			this.getOwnerComponent().getRouter().navTo("dashboard");
+		},
+
+		onSearchIncident: function (oEvent) {
+			var sQuery = oEvent.getParameter("query");
+			var oTable = this._oTable;
+			var oBinding = oTable.getBinding("rows");
+
+			if (sQuery && sQuery.trim() !== "") {
+				// Create filter for Incident ID
+				var oFilter = new Filter("IncidentId", FilterOperator.Contains, sQuery.trim());
+				oBinding.filter([oFilter]);
+			} else {
+				// If search is empty, clear all filters
+				oBinding.filter([]);
+			}
+		},
+
+		onClearSearch: function () {
+			// Clear the search field
+			this._oSearchField.setValue("");
+			
+			// Clear all filters from the table
+			var oTable = this._oTable;
+			var oBinding = oTable.getBinding("rows");
+			oBinding.filter([]);
 		}
 	});
 });
--- ehsm_portal/webapp/controller/Risks.controller.js
+++ ehsm_portal/webapp/controller/Risks.controller.js
@@ -1,13 +1,44 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("ehsmportal.controller.Risks", {
-		onInit: function () {},
+		onInit: function () {
+			// Store reference to the table for filtering
+			this._oTable = this.byId("tblRisks");
+			this._oSearchField = this.byId("riskSearchField");
+		},
 
 		onBack: function () {
 			this.getOwnerComponent().getRouter().navTo("dashboard");
+		},
+
+		onSearchRisk: function (oEvent) {
+			var sQuery = oEvent.getParameter("query");
+			var oTable = this._oTable;
+			var oBinding = oTable.getBinding("rows");
+
+			if (sQuery && sQuery.trim() !== "") {
+				// Use EQ (equals) filter for exact Node ID match
+				var oFilter = new Filter("NodeId", FilterOperator.EQ, sQuery);
+				oBinding.filter([oFilter]);
+			} else {
+				// Clear filter if search is empty
+				oBinding.filter([]);
+			}
+		},
+
+		onClearRiskSearch: function () {
+			// Clear the search field
+			this._oSearchField.setValue("");
+			
+			// Clear the table filter
+			var oTable = this._oTable;
+			var oBinding = oTable.getBinding("rows");
+			oBinding.filter([]);
 		}
 	});
 });
